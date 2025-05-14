81d4949ed5f4f3bed2cb002aca8682b226193e43
robby-sf <robbyrobby9889@gmail.com>
2025-05-14 19:37:43 +0700

    penyesuaian nav bar dan link

--- resources/views/SignUp.blade.php
+++ resources/views/SignUp.blade.php
@@ -4,8 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
   <link rel="icon" type="png" href="/Asset/Logo Tripnesia.PNG">
-  <link href="./Style.css" rel="stylesheet">
+  @vite('resources/css/app.css')
   <title>Tripnesia</title>
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/Page', function () {
+    return view('Page');
+});
+
+Route::get('/destination', function () {
+    return view('destination');
+});
+
+Route::get('/event', function () {
+    return view('event');
+});
+
+Route::get('/package', function () {
+    return view('package');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
