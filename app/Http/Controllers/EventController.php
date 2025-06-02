6babb055f24ff7f3e290a378397c1889d01d52c8
shfzlaa <shofiauliaaa76@gmail.com>
2025-06-02 09:22:41 +0700

    save  controllers

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Event::all();
+        return view('events.index', compact('events'));
+    }
+
+    public function show($slug)
+    {
+        $event = Event::where('slug', $slug)->firstOrFail();
+        return view('events.show', compact('event'));
+    }
 }
