e2bcc493c1c5b91a49fa70e2d04463e37906d049
shfzlaa <shofiauliaaa76@gmail.com>
2025-06-04 00:12:54 +0700

    tes lagi

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -12,7 +12,7 @@
     <h1 class="text-3xl font-bold mb-8 text-center">Daftar Event Tripnesia</h1>
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      @foreach ($events as $event)
+      @foreach ($event as $event)
         <div class="bg-white shadow-lg rounded-lg p-4">
           <h2 class="text-xl font-semibold mb-2">
             <a href="{{ route('events.show', $event->slug) }}" class="text-blue-600 hover:underline">
